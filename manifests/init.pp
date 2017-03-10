86999f9589df65a4e99c153a4abc17103c7298b7
Coto Cisternas <cotocisternas@gmail.com>
2017-03-10 14:32:13 -0300

    backported

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,107 +1,68 @@
 class netdata (
-  String $release_version,
-  Stdlib::Absolutepath $config_dir,
-  Stdlib::Absolutepath $config_file,
-  Integer $firewall_port,
-  Boolean $custom_registry_enabled,
-  Optional[String] $custom_registry_hostname,
-  Optional[String] $custom_registry_to_announce,
-  Boolean $manage_config,
-  Boolean $manage_firewall,
-  Boolean $manage_alarms,
-  Boolean $install_dependencies,
-  Boolean $install_plugin_dependencies,
-  Boolean $install_jq,
-  Optional[String] $install_dir_root,
-  String $installation_source,
-  Optional[String] $repo_location,
-  String $repo_ensure,
-  Boolean $service_enable,
-  String $service_ensure,
-  Boolean $service_manage,
-  String $service_name,
-  Optional[String] $service_provider,
-  Optional[Hash] $options,
-  Stdlib::Absolutepath $alarms_notify_config,
-  Optional[Boolean] $alarms_email,
-  Optional[String] $alarms_email_recipient,
-  Optional[Boolean] $alarms_pushover,
-  Optional[String] $alarms_pushover_recipient,
-  Optional[String] $alarms_pushover_app_token,
-  Optional[Boolean] $alarms_pushbullet,
-  Optional[String] $alarms_pushbullet_access_token,
-  Optional[String] $alarms_pushbullet_recipient,
-  Optional[Boolean] $alarms_twilio,
-  Optional[String] $alarms_twilio_account_sid,
-  Optional[String] $alarms_twilio_account_token,
-  Optional[String] $alarms_twilio_number,
-  Optional[String] $alarms_twilio_recipient,
-  Optional[Boolean] $alarms_messagebird,
-  Optional[String] $alarms_messagebird_access_key,
-  Optional[String] $alarms_messagebird_number,
-  Optional[String] $alarms_messagebird_recipient,
-  Optional[Boolean] $alarms_telegram,
-  Optional[String] $alarms_telegram_bot_token,
-  Optional[String] $alarms_telegram_recipient,
-  Optional[Boolean] $alarms_slack,
-  Optional[String] $alarms_slack_webhook_url,
-  Optional[String] $alarms_slack_recipient,
-  Optional[Boolean] $alarms_discord,
-  Optional[String] $alarms_discord_webhook_url,
-  Optional[String] $alarms_discord_recipient,
-  Optional[Boolean] $alarms_hipchat,
-  Optional[String] $alarms_hipchat_server,
-  Optional[String] $alarms_hipchat_auth_token,
-  Optional[String] $alarms_hipchat_recipient,
-  Optional[Boolean] $alarms_kafka,
-  Optional[String] $alarms_kafka_url,
-  Optional[String] $alarms_kafka_sender_ip,
-  Optional[Boolean] $alarms_pagerduty,
-  Optional[String] $alarms_pagerduty_recipient,
-
-  Hash $purge,
-
-  Hash $python_options,
-
+  $release_version,
+  $config_dir,
+  $config_file,
+  $firewall_port,
+  $custom_registry_enabled,
+  $custom_registry_hostname,
+  $custom_registry_to_announce,
+  $manage_config,
+  $manage_firewall,
+  $manage_alarms,
+  $install_dependencies,
+  $install_plugin_dependencies,
+  $install_jq,
+  $install_dir_root,
+  $installation_source,
+  $repo_location,
+  $repo_ensure,
+  $service_enable,
+  $service_ensure,
+  $service_manage,
+  $service_name,
+  $service_provider,
+  $options,
+  $alarms_notify_config,
+  $alarms_email,
+  $alarms_email_recipient,
+  $alarms_pushover,
+  $alarms_pushover_recipient,
+  $alarms_pushover_app_token,
+  $alarms_pushbullet,
+  $alarms_pushbullet_access_token,
+  $alarms_pushbullet_recipient,
+  $alarms_twilio,
+  $alarms_twilio_account_sid,
+  $alarms_twilio_account_token,
+  $alarms_twilio_number,
+  $alarms_twilio_recipient,
+  $alarms_messagebird,
+  $alarms_messagebird_access_key,
+  $alarms_messagebird_number,
+  $alarms_messagebird_recipient,
+  $alarms_telegram,
+  $alarms_telegram_bot_token,
+  $alarms_telegram_recipient,
+  $alarms_slack,
+  $alarms_slack_webhook_url,
+  $alarms_slack_recipient,
+  $alarms_discord,
+  $alarms_discord_webhook_url,
+  $alarms_discord_recipient,
+  $alarms_hipchat,
+  $alarms_hipchat_server,
+  $alarms_hipchat_auth_token,
+  $alarms_hipchat_recipient,
+  $alarms_kafka,
+  $alarms_kafka_url,
+  $alarms_kafka_sender_ip,
+  $alarms_pagerduty,
+  $alarms_pagerduty_recipient,
   ) {
 
   contain netdata::install
   contain netdata::config
   contain netdata::service
-
-  $default_purge_hash={
-    'python.d'  => false,
-    'node.d'    => false,
-    'charts.d'  => false,
-    'health.d'  => false
-  }
-
-  $_purge_python  = $purge['python.d']
-  $_purge_node    = $purge['node.d']
-  $_purge_charts  = $purge['charts.d']
-  $_purge_health  = $purge['health.d']
-
-  file { "${netdata::config_dir}/python.d":
-    ensure  => directory,
-    mode    => '775',
-    owner   => 'netdata',
-    group   => 'netdata',
-    purge   => $_purge_python,
-    recurse => true,
-    force   => true
-  }
-
-  concat { "${netdata::config_dir}/python.d/web_log.conf":
-    owner   => $netdata::service_name,
-    group   => $netdata::service_name,
-    mode    => '0660',
-  }
-
-  $python_options.each |$name, $params| {
-    ::netdata::pythond{$name:
-      params => $params
-    }
-  }
 
   Class['::netdata::install'] ->
   Class['::netdata::config'] ~>
