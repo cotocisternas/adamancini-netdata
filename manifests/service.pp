308fb7254d31029da6f70304dc9415d771a6face
Coto Cisternas <cotocisternas@gmail.com>
2017-03-12 12:10:15 -0300

    netdata update

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,37 +1,21 @@
 class netdata::service inherits netdata {
 
-  if $netdata::service_manage {
+  file { '/etc/init.d/netdata':
+    ensure  => present,
+    source  => 'puppet:///modules/netdata/netdata.init',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Exec['install_netdata'],
+  }
 
-    case $::facts['service_provider'] {
-
-      'upstart': {
-        exec { 'Install init.d/upstart compatible service file':
-          command => "cp /opt/netdata/${netdata::service_file} ./netdata",
-          cwd     => '/etc/init.d/',
-          creates => '/etc/init.d/netdata',
-          before  => File['/etc/init.d/netdata'],
-        }
-        file { '/etc/init.d/netdata':
-          ensure => present,
-          owner  => 'root',
-          group  => 'root',
-          mode   => '0755',
-        }
-      }
-    }
-
-    if ! ($netdata::service_ensure in [ 'running', 'stopped' ]) {
-      fail('service_ensure parameter must be running or stopped')
-    }
-
-    service { 'netdata':
-      ensure     => $netdata::service_ensure,
-      enable     => $netdata::service_enable,
-      name       => $netdata::service_name,
-      provider   => $netdata::service_provider,
-      subscribe  => File['netdata_config'],
-      hasstatus  => true,
-      hasrestart => true,
-    }
+  service { 'netdata':
+    ensure     => $netdata::service_ensure,
+    enable     => $netdata::service_enable,
+    name       => $netdata::service_name,
+    subscribe  => File['netdata_config'],
+    require    => File['/etc/init.d/netdata'],
+    hasstatus  => true,
+    hasrestart => true,
   }
 }
